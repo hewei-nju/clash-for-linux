0fae5185adeff55d1072bf057e34a8fd0b5960ff
BrightHewei <61347887+hewei-nju@users.noreply.github.com>
2021-10-20 22:48:34 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-#加载系统函数库
-[ -f /etc/init.d/functions ] && source /etc/init.d/functions
+# 加载系统函数库(Only for RHEL Linux)
+# [ -f /etc/init.d/functions ] && source /etc/init.d/functions
+
+# 自定义action函数，实现通用action功能
+success() {
+  echo -en "\\033[60G[\\033[1;32m  OK  \\033[0;39m]\r"
+  return 0
+}
+
+failure() {
+  local rc=$?
+  echo -en "\\033[60G[\\033[1;31mFAILED\\033[0;39m]\r"
+  [ -x /bin/plymouth ] && /bin/plymouth --details
+  return $rc
+}
+
+action() {
+  local STRING rc
+  
+  STRING=$1
+  echo -n "$STRING "
+  shift
+  "$@" && success $"$STRING" || failure $"$STRING"
+  rc=$?
+  echo
+  return $rc
+}
 
 # 判断命令是否正常执行 函数
 if_success() {
